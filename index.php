66c889d3d1ecbcb266b93ff8a992180fc1ec3fd3
Moisés Alex de Castro Macedo <91707010+moises-macedo@users.noreply.github.com>
2022-05-05 11:00:06 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="./Assests/css/styles.css">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+    <header class="header">
+        <h1>Curso PHP</h1>
+        <h2>Índice dos Exercícios</h2>
+    </header>
+    <main class="main">
+        <div class="content">
+            <nav class="modulos">
+                <div class="modulo">
+                    <h3>Módulo 01</h3>
+                    <ul>
+                        <li>
+                            <a href="#">Exercício A</a>
+                        </li>
+                        <li>
+                            <a href="#">Exercício B</a>
+                        </li>
+                        <li>
+                            <a href="#">Exercício C</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo 02</h3>
+                    <ul>
+                        <li>
+                            <a href="#">Exercício A</a>
+                        </li>
+                        <li>
+                            <a href="#">Exercício B</a>
+                        </li>
+                        <li>
+                            <a href="#">Exercício C</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo 03</h3>
+                    <ul>
+                        <li>
+                            <a href="#">Exercício A</a>
+                        </li>
+                        <li>
+                            <a href="#">Exercício B</a>
+                        </li>
+                        <li>
+                            <a href="#">Exercício C</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo 04</h3>
+                    <ul>
+                        <li>
+                            <a href="#">Exercício A</a>
+                        </li>
+                        <li>
+                            <a href="#">Exercício B</a>
+                        </li>
+                        <li>
+                            <a href="#">Exercício C</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo 05</h3>
+                    <ul>
+                        <li>
+                            <a href="#">Exercício A</a>
+                        </li>
+                        <li>
+                            <a href="#">Exercício B</a>
+                        </li>
+                        <li>
+                            <a href="#">Exercício C</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo 06</h3>
+                    <ul>
+                        <li>
+                            <a href="#">Exercício A</a>
+                        </li>
+                        <li>
+                            <a href="#">Exercício B</a>
+                        </li>
+                        <li>
+                            <a href="#">Exercício C</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo 07</h3>
+                    <ul>
+                        <li>
+                            <a href="#">Exercício A</a>
+                        </li>
+                        <li>
+                            <a href="#">Exercício B</a>
+                        </li>
+                        <li>
+                            <a href="#">Exercício C</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo 08</h3>
+                    <ul>
+                        <li>
+                            <a href="#">Exercício A</a>
+                        </li>
+                        <li>
+                            <a href="#">Exercício B</a>
+                        </li>
+                        <li>
+                            <a href="#">Exercício C</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo 09</h3>
+                    <ul>
+                        <li>
+                            <a href="#">Exercício A</a>
+                        </li>
+                        <li>
+                            <a href="#">Exercício B</a>
+                        </li>
+                        <li>
+                            <a href="#">Exercício C</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>            
+        </div>
+    </main>
+    <footer class="footer">
+        Moiśes Alex &copy; <?= date("Y");?>        
+    </footer>
+</body>
+
+</html>
